1e6bd8471f5f4a243336819ec0c8a161ef5d4497
Breno Augusto <brenoaugusto98@gmail.com>
2022-06-13 22:01:27 -0300

    create and delete task features updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
-import './global.scss'
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-import styles from './App.module.scss';
 import { Header } from './components/Header';
-import { AddTaskInput } from './components/AddTaskInput';
 import { Tasks } from './components/Tasks';
+import { PlusCircle } from 'phosphor-react';
+
+import './global.scss'
+import styles from './App.module.scss';
+
+export interface Task {
+  id: string;
+  text: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [ tasks, setTasks ] = useState<Task[]>([]);
+  const [ newTask, setNewTask] = useState('');
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    const task = {
+      id: `random-id-${Math.random()}`,
+      text: newTask,
+      isCompleted: false,
+    }
+
+    setTasks([...tasks, task])
+    setNewTask('');
+  }
+
+  function handleDeleteTask(id: string) {
+    setTasks((state) => state.filter((task) => task.id !== id));
+  }
+
   return (
     <>
       <Header/>
-      <div className={styles.wrapper}>
-        <AddTaskInput/>
-
-        <Tasks/>
-      </div>
+        <div className={styles.wrapper}>
+          <form className={styles.input} onSubmit={handleCreateNewTask} >
+            <input 
+              placeholder="Adicionar uma nova tarefa"
+              value={newTask}
+              onChange={handleNewTaskChange}
+            />
+            <button type="submit">
+              Criar
+              <PlusCircle size={20}/>
+            </button>
+          </form>
+          
+          <Tasks tasks={tasks} onDeleteTask={handleDeleteTask} />
+        </div>
     </> 
   )
 }
--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,25 +1,45 @@
 import styles from './styles.module.scss';
 import clipboardImg from '../../assets/clipboard.svg';
-import { TasksItem } from '../TasksItem';
+import { Task } from '../../App';
+import { Trash } from 'phosphor-react';
 
-export function Tasks() {
-  const hasTasks = true;
+interface TasksProps {
+  tasks: Task[],
+  onDeleteTask: ( id: string ) => void;
+}
+
+export function Tasks({ tasks, onDeleteTask }: TasksProps ) {
+  const hasTasks = tasks.length;
   
   return (
     <>
       <header className={styles.header}>
         <strong>
-          Tarefas criadas <span> 0 </span>
+          Tarefas criadas <span>{tasks.length}</span>
         </strong>
         <strong>
-          Concluídas <span> 0 </span>
+          Concluídas <span> 0 de {tasks.length} </span>
         </strong>
       </header>
 
       { hasTasks ? (
         <>
-          <TasksItem/>
-          <TasksItem/>
+          {tasks.map(task => {
+            return (
+              <main className={styles.taskItem} key={task.id} >
+                <div>
+                  <input id={`checkbox-${task.id}`} type="checkbox"/> 
+                  <label htmlFor={`checkbox-${task.id}`}/>
+                </div>
+                <p className={ task.isCompleted ? styles.selected : ''} >
+                  {task.text}
+                </p>
+                <button type="button" onClick={() => onDeleteTask(task.id)} >
+                  <Trash/>  
+                </button>  
+             </main>
+            )
+          })}
         </>
       ) : (
         <section className={styles.emptyTasks} >
